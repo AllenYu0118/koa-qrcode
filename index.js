c019554ae0750a610286b6520f09d2d2ed285cf7
yulei <yulei@addcn.com>
2019-03-25 23:21:55 +0800

    add options

--- index.js
+++ index.js
@@ -3,24 +3,19 @@ const path = require('path')
 const Koa = require('koa')
 const route = require('koa-route')
 const serve = require('koa-static')
+const onerror = require('koa-onerror');
 var QRCode = require('qrcode')
 
 const app = new Koa()
 
+onerror(app)
+
 const staticPath = serve(path.join(__dirname, 'public'));
 
 // With async/await
-const generateQR = async text => {
-    try {
-        let data = await QRCode.toDataURL(text)
-        return data
-    } catch (err) {
-        console.error(err)
-    }
+const generateQR = async (text, options) => {
+    return await QRCode.toDataURL(text, options)
 }
-
-
-
 
 const qrcode = async ctx => {
     const query = ctx.query // 查询参数
@@ -28,15 +23,25 @@ const qrcode = async ctx => {
         console.log('请传递data数据')
     }
 
-    return await generateQR(query.data)
-}
+    let { size, margin, scale, dark, light } = query
 
+    return await generateQR(query.data, {
+        width: size,
+        margin,
+        scale,
+        color: {
+            dark,
+            light
+        }
+    })
+}
 
 
 const logger = async (ctx, next) => {
     console.log(`${Date.now()} ${ctx.request.method} ${ctx.request.url}`);
     await next()
 }
+
 app.use(logger)
 app.use(route.get('/', async (ctx, next) => {
     ctx.response.type = 'html'
@@ -53,5 +58,5 @@ app.use(route.get('/qrcode', async (ctx, next) => {
 app.use(staticPath)
 
 app.listen(9527, () => {
-    console.log(`服务已开启, 端口 9527`)
+    console.log(`服务已开启, 端口 9527, https:localhost:9527`)
 })
